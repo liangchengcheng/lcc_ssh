d6d179bf04cb7ab970170b13e66448392b6cf5fe
liangchengcheng <1038127753@qq.com>
2016-09-05 08:32:51 +0800

    change action code and add view layout
    change action code and add view layout

--- src/com/lcc/action/EmployeeAction.java
+++ src/com/lcc/action/EmployeeAction.java
@@ -3,6 +3,7 @@ package com.lcc.action;
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
+import java.util.Date;
 import java.util.Map;
 
 import org.apache.struts2.interceptor.RequestAware;
@@ -68,16 +69,61 @@ public class EmployeeAction extends ActionSupport implements RequestAware,ModelD
 		return "ajax-success";
 	}
 	
-	@Override
-	public void prepare() throws Exception {
-		// TODO Auto-generated method stub
-		
+	public String input(){
+		request.put("departments", departmentService.getAll());
+		return INPUT;
 	}
+	
+	public void prepareInput(){
+		if(id != null){
+			model = employeeService.get(id);
+		}
+	}
+	
+	public String save(){
+		if(id == null){
+			model.setCreateTime(new Date());			
+		}
+		employeeService.saveOrUpdate(model);
+		return SUCCESS;
+	}
+	
+	/**
+	 * 可以根据 id 来判断为 save 方法准备的 model 是 new 的还是从数据库获取的!
+	 */
+	public void prepareSave(){
+		if(id == null){
+			model = new Employee();
+		}else{
+			model = employeeService.get(id);
+		}
+	}
+	
+	private String lastName;
+	
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	
+	public String validateLastName() throws UnsupportedEncodingException{
+		if(employeeService.lastNameIsValid(lastName)){
+			inputStream = new ByteArrayInputStream("1".getBytes("UTF-8")); 
+		}else{
+			inputStream = new ByteArrayInputStream("0".getBytes("UTF-8")); 
+		}
+		
+		return "ajax-success";
+	}
+	
+	@Override
+	public void prepare() throws Exception {	
+	}
+	
+	private Employee model;
 
 	@Override
 	public Employee getModel() {
-		// TODO Auto-generated method stub
-		return null;
+		return model;
 	}
 
 }
